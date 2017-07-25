aac9663d21ad662c58345afa1a9b924d26067cda
Pavel Cahyna <pcahyna@users.noreply.github.com>
2017-07-25 19:12:31 +0200

    WIP: Adapt one actor to the simplified framework (Will not work yet, the  framework is not yet ready.) XXX the YAML file should be empty and 'out' be the default output port

--- src/leappto/scripts/connectivity/actordecl.py
+++
@@ -1,4 +0,0 @@
-from leappto.actor_support.portannotation import PortAnnotation, DstPortAnnotation, InitialPortAnnotation, Any
-from leappto.msgtypes.msgtypes import ConnectivityStatus, Trigger
-
-outports_annotations = {'connectivity_status': PortAnnotation(ConnectivityStatus)}
